a80cbdc4e6afeddf539880578933adbbd4a1de8d
moonee <hellomooneekim@gmail.com>
2021-07-25 22:11:04 +0900

    feat : 헤더,푸터 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { Component, ReactElement } from "react";
 import Header from "components/Header";
+import Footer from "components/Footer";
 
 function App() {
   return (
     <div className="layout">
       <Header />
+      <main>음</main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <a href="https://github.com/moonheekim0118">@moonheekim0118</a> 만듦
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
