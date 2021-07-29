304f7220ad1e28acf9fdfee7befc08908a96d990
moonee <hellomooneekim@gmail.com>
2021-07-29 10:23:31 +0900

    chore : 빌드 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
   },
   resolve: {
