6522000cfb8eda7b96c6a119c2a17995690c7d6e
moonee <hellomooneekim@gmail.com>
2021-08-01 12:04:44 +0900

    feat : 캐싱 구현 - 이전에 번역한 결과->캐싱

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,10 @@ const URL = "https://hanbon.herokuapp.com/translate";
 const TIMEOUT = 8000;
 const { signal, abort } = new AbortController();
 
+const cache = new Map();
+
 async function request(query: string) {
+  if (cache.has(query)) return cache.get(query);
   const timer = setTimeout(() => abort(), TIMEOUT);
   const response = await fetch(`${URL}/${query}`, { signal });
   if (!response.ok) {
@@ -10,7 +13,9 @@ async function request(query: string) {
   }
   const data = await response.json();
   clearTimeout(timer);
-  return data.message.result.translatedText;
+  const result = data.message.result.translatedText;
+  cache.set(query, result);
+  return result;
 }
 
 export default request;
