40fc222fa8cecb5ebc3c4c5dcc76efc6bcb6813d
moonee <hellomooneekim@gmail.com>
2021-08-12 15:16:34 +0900

    fix : 초기 로딩 abort 되는 문제 해결

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,14 @@
 import { ERROR_MESSAGE } from "@constants/index";
 const URL = process.env.API_URL;
-const TIMEOUT = 8000;
-const controller = new AbortController();
-const signal = controller.signal;
-
 const cache = new Map();
 
 async function request(query: string) {
   if (cache.has(query)) return cache.get(query);
-  const timer = setTimeout(() => {
-    controller.abort();
-    throw new Error(ERROR_MESSAGE);
-  }, TIMEOUT);
-  const response = await fetch(`${URL}/${query}`, { signal });
+  const response = await fetch(`${URL}/${query}`);
   if (!response.ok) {
     throw new Error(ERROR_MESSAGE);
   }
   const data = await response.json();
-  clearTimeout(timer);
   const result = data.message.result.translatedText;
   cache.set(query, result);
   return result;
